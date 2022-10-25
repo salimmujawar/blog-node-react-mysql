b9bd5aba899321f4dd7f710c92b52343111ed76f
salimmujawar <salim685@gmail.com>
2022-10-25 10:57:45 +0400

    Adding React Router Dom 6.4

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,  
+  Route,
+  Outlet,  
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/post/:id",
+        element:<Single/>
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      }
+    ]
+  },
+  {
+    path:"/register",
+    element:<Register/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  }
+  ,
+  {
+    path:"/write",
+    element:<Write/>
+  }
+  ,
+  {
+    path:"/single",
+    element:<Single/>
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router}/>
+      </div>      
     </div>
   );
 }
