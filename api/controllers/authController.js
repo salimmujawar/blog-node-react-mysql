3f495ab664034c220736ddab303fc77e9a277d7c
salimmujawar <salim685@gmail.com>
2022-10-26 16:30:24 +0400

    Node added Mysql flow and Bcrypt module

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,34 @@
 import {db} from "../config/db.js"
+import bcrypt from "bcrypt"
 
 export const register = (req, res) => {
-  res.json("from register");
+  //CHECK USER EXIST
+  const sql = "SELECT * FROM users WHERE email = ? OR USERNAME = ?";
+
+  db.query(sql, [req.body, req.body.username], (err, data) => {
+      if(err) {
+        return res.json(err);
+      }
+      if(data.length){
+        return res.status(409).json("User already exist!");
+      }
+      //Hash the password and create the users
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+      const insertSql = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)"
+      const values = [
+        req.body.username,
+        req.body.email,
+        hash
+      ]
+      db.query(insertSql, values, (err, data) => {
+        if(err) {
+          return res.json(err);
+        }else {
+          return res.status(200).json("User has been created!!!")
+        }
+      });
+  });
 }
 
 export const login = (req, res) => {
