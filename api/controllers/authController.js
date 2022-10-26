dea25cadeb097c443f813ca15ae7a2cf4636a6f6
salimmujawar <salim685@gmail.com>
2022-10-26 16:19:42 +0400

    Added Routes Controll Structure

---
+++ api/controllers/authController.js
@@ -0,0 +1,13 @@
+import {db} from "../config/db.js"
+
+export const register = (req, res) => {
+  res.json("from register");
+}
+
+export const login = (req, res) => {
+  
+}
+
+export const logout = (req, res) => {
+  
+}
--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express from "express";
+import routes from "./routes/web.js";
 
 const app =express()
 
 app.use(express.json())
 
+
+
+
+//init all web routes
+app.use("/api/posts", routes);
+
 let port = process.env.PORT || 8080;
-
-
-
 app.listen(port, () => {
   console.log(`Listinign to port: ${port}`);
 })
